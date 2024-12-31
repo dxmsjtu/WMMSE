a98055fa1771d85ea10dc9b5529c4461efc086fe
dxm <dxmsjtu@sohu.com>
2024-12-31 17:07:35 +0800

    更新参考文献和注释
    更新参考文献和注释

--- find_U.m
+++ find_U.m
@@ -1,13 +1,13 @@
 function U = find_U(H,V,sigma2, P, R,I,d)
-
+%[1] An Iteratively Weighted MMSE Approach to Distributed Sum-Utility Maximization for a 
     J = zeros(R,R,I);  %计算不含噪声项的矩阵
     U = zeros(R,d,I);
 
     for i=1:I
             for l=1:I
                     J(:,:,i) = J(:,:,i) + H(:,:,i)*V(:,:,l)*(V(:,:,l)')*(H(:,:,i)'); 
-            end
+            end% line4 of table I of [1] 
   
-            U(:,:,i) = (J(:,:,i) + sigma2*eye(R,R)) \ (H(:,:,i)*V(:,:,i)); 
+            U(:,:,i) = (J(:,:,i) + sigma2*eye(R,R)) \ (H(:,:,i)*V(:,:,i)); % line4 of table I of [1] 
     end     
 end
--- find_W.m
+++ find_W.m
@@ -1,4 +1,5 @@
 function W = find_W(U,H,V, R, I,d)
+%[1]An Iteratively Weighted MMSE Approach to Distributed Sum-Utility Maximization for a MIMO Interfering Broadcast Channel
     W = zeros(d,d,I);
     for i=1:I
             W(:,:,i) = inv(eye(d)-U(:,:,i)'*H(:,:,i)*V(:,:,i)); 
